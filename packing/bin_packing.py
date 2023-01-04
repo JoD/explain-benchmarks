f35f589f34a214eb726469ffece13fcc03ab8ef4
Ignace Bleukx <ignace.bleukx@kuleuven.be>
2023-01-04 22:01:22 +0100

    bin_packing generator

--- packing/bin_packing.py
+++ packing/bin_packing.py
@@ -1,17 +1,31 @@
 
 import random
 
-def generate_packing_problem(n_items, width, seed=0):
+def generate_packing_problem(n_items, bin_width, seed=0):
 
     random.seed(seed)
 
-    lb, ub = 1, width
+    lb, ub = 1, bin_width // 2
 
     widths = [random.randint(lb,ub) for _ in range(n_items)]
     heights = [random.randint(lb,ub) for _ in range(n_items)]
 
-    return {"bin_width": width,
+    return {"bin_width": bin_width,
             "widths" :widths,
             "heights":heights
             }
 
+
+def generate_2d_packing_problem(n_items, seed=0):
+
+    random.seed(seed)
+
+    lb,ub = 1,10
+
+    widths = [random.randint(lb, ub) for _ in range(n_items)]
+    heights = [random.randint(lb, ub) for _ in range(n_items)]
+
+    return {"bin_width": sum(widths),
+            "widths": widths,
+            "heights": heights
+            }
