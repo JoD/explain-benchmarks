601f9855e98e8bd98526d20ff73be98352756bbd
Ignace Bleukx <ignace.bleukx@kuleuven.be>
2023-02-22 14:09:57 +0100

    helper function for unsat sudokus

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
 from .jobshop.jobshop import generate_instance as generate_jobshop
 from .sudoku.sudoku import model_unsat_sudoku as generate_unsat_sudoku
+from .sudoku.sudoku import load_and_make_unsat_sudoku
 from .packing.bin_packing import generate_packing_problem as generate_bin_packing, generate_2d_packing_problem
