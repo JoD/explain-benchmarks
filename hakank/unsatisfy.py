31a5c235e252dabaa699d7b426db5e997f86b5f6
Ignace Bleukx <ignace.bleukx@kuleuven.be>
2023-02-24 17:01:48 +0100

    more improvements to unsatisfy

--- hakank/unsatisfy.py
+++ hakank/unsatisfy.py
@@ -5,7 +5,10 @@ import cpmpy as cp
 
 from cpmpy.transformations.get_variables import get_variables
 from cpmpy.expressions.core import Operator, Comparison
-from cpmpy.expressions.utils import is_any_list
+from cpmpy.expressions.variables import _BoolVarImpl
+from cpmpy.expressions.globalconstraints import *
+from cpmpy.expressions.utils import is_any_list, flatlist
+from cpmpy.solvers.solver_interface import ExitStatus
 
 TIME_LIMIT = 60
 
@@ -13,49 +16,66 @@ TIME_LIMIT = 60
 def make_model_unsat(constraints, p_change=0.1, seed=0):
     random.seed(seed)
 
-    model = cp.Model(constraints)
-    callback = callback_factory(model)
+    start_time = time()
+    callback = callback_factory(constraints)
 
-    while callback():
-        for cons in constraints:
-            if callback():
-                change_constraint_with_prob(cons, p_change,
-                                            should_continue=callback)
+    it = 0
+    while callback(TIME_LIMIT - (time() - start_time)):
+        print(f"\rAt iteration {it}", end="\t\t\t")
+        for i, cons in enumerate(list(constraints)):
+            if time() - start_time >= TIME_LIMIT:
+                print()
+                raise TimeoutError("Make model unsat timed out")
+            if callback(TIME_LIMIT - (time() - start_time)):
+                changed_cons = change_constraint_with_prob(cons,
+                                                            p_change,
+                                                            should_continue=callback,
+                                                            time_limit=TIME_LIMIT - (time() - start_time))
+                constraints[i] = changed_cons
 
+        it += 1
+    print()
     return constraints
 
-def callback_factory(model):
+def callback_factory(constraints):
 
     history = dict()
-    def callback():
-        print(model)
-        if str(model.constraints) not in history:
-            history[str(model.constraints)] = model.solve()
-        return history[str(model.constraints)]
+    def callback(time_limit):
+        # print(constraints)
+        if str(constraints) not in history:
+            # solving model
+            model = cp.Model(constraints)
+            res = model.solve(time_limit=int(time_limit))
+            if model.status().exitstatus == ExitStatus.UNKNOWN:
+                raise TimeoutError("Callback timed out")
+            history[str(constraints)] = res
+        return history[str(constraints)]
 
     return callback
 
 
-def change_constraint_with_prob(cpm_expr, p_change=0.1, should_continue=lambda:False):
+def change_constraint_with_prob(cpm_expr, p_change=0.1, should_continue=lambda:False, time_limit=60):
 
-    if is_any_list(cpm_expr):
-        [change_constraint_with_prob(e) for e in cpm_expr]
+    start_time = time()
 
     try:
         # sample action
-        if not should_continue():
-            return
+        if not should_continue(time_limit - (time() - start_time)):
+            return cpm_expr
         action = random.choice([swap_variables, replace_constraint])
         if random.random() <= p_change:
-            action(cpm_expr)
+            cpm_expr = action(cpm_expr)
     except RuleNotApplicableError:
         # chosen rule was not applicable, continue
         pass
+
     if hasattr(cpm_expr, "args"):
-        for arg in cpm_expr.args:
-            change_constraint_with_prob(arg, p_change, should_continue)
+        for i, arg in enumerate(cpm_expr.args):
+            if time() - start_time >= TIME_LIMIT:
+                raise TimeoutError("change constraint timed out")
+            cpm_expr.args[i] = change_constraint_with_prob(arg, p_change, should_continue, time_limit=time_limit - (time() - start_time))
 
-
+    return cpm_expr
 
 
 
@@ -68,7 +88,14 @@ def swap_variables(cpm_expr, var1=None, var2=None):
         vars = get_variables(cpm_expr)
         if len(vars) <= 1:
             raise RuleNotApplicableError()
-        var1, var2 = random.choices(vars, k=2)
+        var1 = random.choice(vars)
+        vars = [var for var in vars if str(var) != str(var1) and var.is_bool() == var1.is_bool()]
+        if len(vars) <= 0:
+            raise RuleNotApplicableError()
+        var2 = random.choice(vars)
+
+    assert (isinstance(var1, _BoolVarImpl) and isinstance(var2, _BoolVarImpl)) or \
+           (not isinstance(var1, _BoolVarImpl) and not isinstance(var2, _BoolVarImpl))
 
     # if we change the names of variables inplace, it will also change in other constraints...
     #   this might actually undo previous swaps... so lets replace the variables in this constraint only
@@ -80,14 +107,19 @@ def swap_variables(cpm_expr, var1=None, var2=None):
             elif str(arg) == str(var2):
                 cpm_expr.args[i] = var1
             else:
-                swap_variables(arg, var1, var2)
+                cpm_expr.args[i] = swap_variables(arg, var1, var2)
+
+    return cpm_expr
 
 
 def replace_constraint(cpm_expr):
     if isinstance(cpm_expr, Operator):
-        replace_operator(cpm_expr)
+        return replace_operator(cpm_expr)
     if isinstance(cpm_expr, Comparison):
-        replace_comparison(cpm_expr)
+        return replace_comparison(cpm_expr)
+    if isinstance(cpm_expr, GlobalConstraint):
+        return replace_global(cpm_expr)
+    return cpm_expr
 
 def replace_operator(cpm_expr):
     """
@@ -98,23 +130,26 @@ def replace_operator(cpm_expr):
         raise RuleNotApplicableError()
 
     arity, is_bool = Operator.allowed[cpm_expr.name]
-    if arity == 0:
-        arity = len(cpm_expr.args) # so we also allow 'and' to be replaced with -> for example
+    if cpm_expr.name == "wsum":
+        arity = 0
 
     options = []
     for n, (a,ib) in Operator.allowed.items():
-        if (a == arity or a == 0) and cpm_expr.name != n and ib == is_bool and \
-                n != "mod" and n != "pow" and n != "div": # these operators are annoying
+        arity_matches = a == arity or a == 0 or n == "wsum"
+        excl_opertors = n != "mod" and n != "pow" and n != "div" and n != cpm_expr.name
+        if (arity_matches and excl_opertors and ib == is_bool):
             options += [n]
 
     new_op = random.choice(options)
     if new_op == "wsum":
         weights = [random.randint(0,100) for _ in cpm_expr.args]
-        cpm_expr.args = [weights, cpm_expr.args]
+        args = [weights, cpm_expr.args]
     elif cpm_expr.name == "wsum":
-        cpm_expr.args = cpm_expr.args[1] # drop weights
+        args = cpm_expr.args[1] # drop weights
+    else:
+        args = cpm_expr.args
 
-    cpm_expr.name = new_op
+    return Operator(name=new_op, arg_list=args)
 
 
 def replace_comparison(cpm_expr):
@@ -125,13 +160,101 @@ def replace_comparison(cpm_expr):
         raise RuleNotApplicableError()
 
     options = [n for n in Comparison.allowed if n != cpm_expr.name]
-    cpm_expr.name = random.choice(options)
+    new_name = random.choice(options)
+    return Comparison(new_name, *cpm_expr.args)
 
+def replace_global(cpm_expr):
+    """
+        Replace global constraint with another one
+    """
 
+    if not isinstance(cpm_expr, GlobalConstraint):
+        raise RuleNotApplicableError()
 
+    # element constraint, introduce off-by-1 error
+    if cpm_expr.name == "element":
+        return Element(cpm_expr.args[0], cpm_expr.args[1]+1)
+
+    # boolean globals
+    bool_options = [
+        AllDifferent,
+        AllDifferentExcept0,
+        AllEqual,
+        Circuit
+    ]
+    if any(isinstance(cpm_expr, t) for t in bool_options):
+        # arity will match, make new global constraint
+        options = [t for t in bool_options if not isinstance(cpm_expr, t)]
+        new_op = random.choice(options)
+        return new_op(cpm_expr.args)
+
+    # numerical globals
+    num_options = [Maximum, Minimum]
+    if any(isinstance(cpm_expr, t) for t in num_options):
+        # arity will match, make new global constraint
+        options = [t for t in num_options if not isinstance(cpm_expr, t)]
+        new_op = random.choice(options)
+        return new_op(cpm_expr.args)
+
+    return cpm_expr
 
 class RuleNotApplicableError(Exception):
     pass
+
+
+if __name__ == "__main__":
+    import os
+    # from cpmpy import *
+    #
+    # x,y,z = intvar(0,10, shape=3)
+    # a,b = boolvar(shape=2)
+    #
+    # model = Model(
+    #     [AllDifferent(x,y,z),
+    #      a.implies(x + y <= z),
+    #      b.implies(x + y >= z),
+    #      ~a | b]
+    # )
+    #
+    #
+
+    dirname = "pickled"
+    outdir = "pickled_unsat_new"
+    fnames = os.listdir(dirname)
+    already_done = set(os.listdir(outdir))
+
+    skip = { # all of these timed out
+        "a_17b2023_01_11.pkl",
+        "a_2012_CMO_problem2023_01_11.pkl",
+        "bus_scheduling_csplib2023_01_11.pkl",
+        "candies2023_01_11.pkl",
+        "clique2023_01_11.pkl",
+        "equal_sized_groups2023_01_11.pkl",
+        "euler12023_01_11.pkl",
+        "generating_numbers22023_01_11.pkl",
+        "knights_path2023_01_11.pkl",
+        "langford2023_01_11.pkl"
+    }
+
+    already_done |= skip
+
+    for fname in sorted(set(fnames) - already_done):
+        model = cp.Model.from_file(os.path.join(dirname, fname))
+        cons = flatlist(model.constraints)
+        if len(cons) <= 1000:
+            print(f"Making model {fname} unsat. Model has {len(cons)} constraints")
+        else:
+            print(f"Skipping model {fname} as it has too many constraints ({len(cons)})")
+            continue
+
+        try:
+            unsat_cons = make_model_unsat(cons, p_change=0.1,seed=0)
+            unsat_model = cp.Model(unsat_cons)
+            assert unsat_model.solve() is False
+            unsat_model.to_file(os.path.join(outdir, fname))
+        except TimeoutError as e:
+            print(e)
+
 
 
 
